32d06b7f0f4767d8f664847588683d70ec07ebe3
graydragon2 <graydragon2@gmail.com>
2025-07-30 09:29:47 -0400

    Added favicon icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,17 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '@/components/Layout';
 import { ThemeProvider } from 'next-themes';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <Head>
+        <title>ThreatPulse</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon-32x32.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
